53915f4d4e9092f4538f029beb0a851cca5630fb
Ayush Sahay <sahayayush385@gmail.com>
2024-04-10 22:15:22 +0530

    Databse connection error fixed

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,7 @@ conf(); //configuring dotenv
 
 const _config = {
     port: process.env.PORT || 3000,
+    uri: process.env.MONGODB_URI,
 }
 
 export const config = Object.freeze(_config); //making read only
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,33 +1,53 @@
-import mongoose from 'mongoose';
-//user: sahayayush385
-//password: 1aITQkXHypA6z2Nq
+// import mongoose from 'mongoose';
+// //user: sahayayush385
+// //password: 1aITQkXHypA6z2Nq
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = process.env.MONGODB_URI;
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// const connectDB = async () => {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// connectDB().catch(console.dir);
+
+// export default connectDB;
+
+import mongoose from "mongoose";
+import { config } from "./config";
 
 
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGODB_URI;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
+const connectDB = async () => {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    mongoose.connection.on("connected", () => {
+      console.log("Connected to database successfully");
+    });
+
+    mongoose.connection.on("error", (err) => {
+      console.log("Error in connecting to database.", err);
+    });
+
+    await mongoose.connect(config.uri as string);
+  } catch (err) {
+    console.error("Failed to connect to database.", err);
+    process.exit(1);
   }
-}
-run().catch(console.dir);
+};
+
+export default connectDB;
