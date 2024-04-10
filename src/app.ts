1a63dd63653f110bf6644e084dbc500b59b40607
Ayush Sahay <sahayayush385@gmail.com>
2024-04-10 23:26:08 +0530

    user modelling and authentication

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
-// import userRouter from "./user/userRouter";
-// import bookRouter from "./book/bookRouter";
+import userRouter from "./user/userRouter";
+import bookRouter from "./book/bookRouter";
 import { config } from "./config/config";
 
 const app = express();
@@ -21,8 +21,8 @@ app.get("/", (req, res, next) => {
   res.json({ message: "Welcome to elib apis" });
 });
 
-// app.use("/api/users", userRouter);
-// app.use("/api/books", bookRouter);
+app.use("/api/users", userRouter);
+app.use("/api/books", bookRouter);
 
 // Global error handler
 app.use(globalErrorHandler);
